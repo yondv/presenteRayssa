f5c92262281026186f37dc091c3d0615bf0be7b2
Vitor <thevitor.hugo27@gmail.com>
2023-03-07 16:40:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let secao1 = document.getElementById('sec-1');
+let secao2 = document.getElementById('sec-2-neg');
+
+function alohomora(){
+    secao1.id = 'sec-1-neg';
+    secao2.id = 'sec-2';
+    speechSynthesis. speak(new SpeechSynthesisUtterance("O feitiço ALOHOMORA abre portas, caminhos, fechaduras... queria dizer que isso representa a abertura das portas do meu coração pro Espírito Santo, a REAL aceitação de Jesus Cristo na minha vida, e apenas o início de uma tremanda mudança! Tudo o que vamos passar hoje, mesmo que algo fuja dos planos, vai ser incrível, e a gente pode começar com você procurando o que tem de baixo da sua cama. te amo meu amorzinho!"));
+}
